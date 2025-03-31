f64136e291491210f8bf9344f4f225c9685bd490
Sushree Nadiminty <sushreenadiminty@Sushrees-MacBook-Air-2.local>
2025-03-31 14:14:04 -0400

    changes to bind port

--- buffalingo-backend/app.py
+++ buffalingo-backend/app.py
@@ -113,4 +113,4 @@ def evaluate_word():
         return jsonify({"error": "Internal server error", "details": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
